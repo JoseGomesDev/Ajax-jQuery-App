e8ec472ffe4921dd72172166c8a34a9f51f7a6b2
JoseGomesHub <jose.gomes_t8@hotmail.com>
2022-05-04 17:10:26 -0300

    first commit

---
+++ app_dashboard/script.js
@@ -0,0 +1,43 @@
+$(document).ready(() => {
+	
+	$('#documentacao').on('click', () => {
+        $('#pagina').load('documentacao.html')
+    })
+
+    $('#suporte').on('click', () => {
+        //$('#pagina').load('suporte.html')
+
+        $.get('suporte.html', data => {
+            $('#pagina').html(data)
+        })
+    })
+
+    $('#homeagain').on('click', () => {
+        //$('body').load('index.html')
+
+        $.post('index.html', data => {
+            $('body').html(data)
+        })
+    })
+
+
+	//ajax
+	$('#competencia').on('change', e => {
+
+		let competencia = $(e.target).val()
+		
+		$.ajax({
+			type: 'GET',
+			url: 'app.php',
+			data: `competencia=${competencia}`, //x-www-form-urlencoded
+			dataType: 'json',
+			success: dados => { 
+				$('#numeroVendas').html(dados.numeroVendas)
+				$('#totalVendas').html(dados.totalVendas)
+			},
+			error: erro => { console.log(erro) }
+		})
+
+		//método, url, dados, sucesso, erro
+	})
+})
